c625e081214547a9f5fcc431bb0664d236897253
Ronak Bhatt <ronakabhattrz@gmail.com>
2021-06-15 11:34:34 +0530

    #  Cross-Component Communication # Working with State & Types. # Managing State Better. # More Props & State Work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
+
 
 const App: React.FunctionComponent = () => {
-  const todos = [{id: "t1", text: "Finish the Course."}];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const AddTodoHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text: text}]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
 
   return (
     <div className="App">
-      <TodoList items={todos} />
+      <NewTodo onAddTodo={AddTodoHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 }
