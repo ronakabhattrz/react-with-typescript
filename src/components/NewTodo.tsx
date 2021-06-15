3e59db45b3ce57d5002e42e26eed8f5622d88457
Ronak Bhatt <ronakabhattrz@gmail.com>
2021-06-15 11:54:41 +0530

    Add Styling TO componenets

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import './NewTodo.css';
 
 type NewTodoProps = {
     onAddTodo: (todoText: string) => void;
@@ -15,7 +16,7 @@ const NewTodo: React.FC<NewTodoProps> = props => {
 
     return (
         <form onSubmit={TodoSubmitHandller}>
-            <div>
+            <div className="form-control">
                 <label htmlFor="todo-text" >Todo Text</label>
                 <input type="text" id="todo-text" ref={textInputRef} />
             </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TodoList.css';
 
 interface TodoListProps {
     items: {id: string, text: string}[];
@@ -12,7 +13,7 @@ const TodoList: React.FC<TodoListProps> = props => {
             {props.items.map(todo => ( 
                 <li key={todo.id}>
                     <span>{todo.text}</span>
-                    <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE!!!</button>
+                    <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
                 </li>
             ))}
         </ul>
